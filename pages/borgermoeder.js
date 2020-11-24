cb7e46d61099680ea950aa5d7187370d6756ed3c
Johanne Justesen <johannejpoulsen@gmail.com>
2020-11-24 15:23:36 +0100

    Update borgermoeder.js

--- pages/borgermoeder.js
+++ pages/borgermoeder.js
@@ -2,7 +2,7 @@ import hoeringService from "../services/hoeringer.js";
 export default class HoeringerPage {
   constructor() {
     this.template();
-    this.initData();
+    // this.initData();
   }
 
   async initData() {
@@ -21,16 +21,15 @@ export default class HoeringerPage {
   }
 
   appendHoeringer(hoeringer) {
-    console.log(hoeringer);
-   let template = "";
-    for (hoering of hoeringer) {
+    let template = "";
+    hoeringer.forEach((hoering) => {
       template += /*html*/ `
         <article>
         <h2>${hoering.title.rendered}</h2>
         <p>${hoering.content.rendered}</p>
         </article>
         `;
-    }
+    });
     document.querySelector("#grid-hoeringer").innerHTML = template;
   }
-}
+  }
